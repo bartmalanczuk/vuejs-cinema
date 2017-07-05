07086d98c28143736a513a99c97ea1c387693974
Bart <bart.malanczuk@gmail.com>
2017-07-05 15:04:29 +0200

    Bootstrap movie-list and movie-filter

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import Vue from 'vue';
 
 new Vue({
     el: '#app',
-    data: {
-        msg: 'Hello World',
-    },
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                           Movie list
+                       </div>`,
+        },
+        'movie-filter': {
+            template: `<div id="movie-filter">
+                           Movie filter
+                       </div>`,
+        },
+    }
 });
