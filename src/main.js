106d9366a79db8ccf8c4c307f8fd2d619d4dbe78
Bart <bart.malanczuk@gmail.com>
2017-07-08 23:36:31 +0200

    Display movies filtered by genre

--- src/main.js
+++ src/main.js
@@ -23,16 +23,25 @@ new Vue({
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                           <div class="movie" v-for="movie in movies">{{ movie.title }}</div>
+                           <div class="movie" v-for="movie in filteredMovies">{{ movie.title }}</div>
                        </div>`,
+            props: [ 'genre', 'time', ],
             data() {
                 return {
                     movies: [
-                        { title: 'Pulp Fiction' },
-                        { title: 'Home Alone' },
-                        { title: 'Manchaster by the Sea' },
+                        { title: 'Pulp Fiction', genre: genres.CRIME },
+                        { title: 'Home Alone', genre: genres.COMEDY },
+                        { title: 'Manchaster by the Sea', genre: genres.DRAMA },
                     ],
                 };
+            },
+            computed: {
+                filteredMovies() {
+                    if (this.genre.length > 0) {
+                        return this.movies.filter(movie => this.genre.some(genere => genere === movie.genre));
+                    }
+                    return this.movies;
+                },
             },
         },
         'movie-filter': {
