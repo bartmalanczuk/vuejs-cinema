1ab4588e5ed3e096db3edbddbddee1f06e0347c9
Bart <bart.malanczuk@gmail.com>
2017-07-10 22:41:45 +0200

    Filter based on time

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,19 +1,26 @@
 <template>
     <div id="movie-filter">
         <h2>Filter results</h2>
+        <h3>By time of day</h3>
         <div class="filter-group">
-            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+            <check-filter v-for="time in times" category="time" v-bind:title="time" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+        <h3>By genre</h3>
+        <div class="filter-group">
+            <check-filter v-for="genre in genres" category="genre" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
         </div>
     </div>
 </template>
 <script>
     import CheckFilter from './CheckFilter.vue';
     import genres from '../util/genres.js';
+    import times from '../util/times.js';
 
     export default {
         data() {
             return {
                 genres,
+                times,
             };
         },
         methods: {
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -17,16 +17,28 @@
 </template>
 <script>
     import genres from '../util/genres.js';
+    import times from '../util/times.js';
     import MovieItem from './MovieItem.vue';
 
     export default {
         props: [ 'movies', 'genre', 'time', 'day' ],
         computed: {
             filteredMovies() {
-                if (this.genre.length > 0) {
-                    return this.movies.filter(movie => this.genre.some(genre => movie.movie.Genre.match(genre)));
-                }
-                return this.movies;
+                return this.movies.filter((movie) => {
+                    return this.genre.length <= 0 || this.genre.some(genre => movie.movie.Genre.match(genre));
+                }).filter((movie) => {
+                    return movie.sessions.some((session) => {
+                        if (!this.$moment(session.time).isSame(this.day, 'day')) {
+                            return false;
+                        } else if (this.time.length === 0 || this.time.length === 2) {
+                            return true;
+                        } else if (this.time[0] === times.BEFORE_6PM) {
+                            return this.$moment(session.time).hour() <= 18;
+                        } else {
+                            return this.$moment(session.time).hour() >= 18;
+                        }
+                    });
+                });
             },
         },
         components: {
