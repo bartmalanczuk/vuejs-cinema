dce951e06d5240dca06d7e2e69fd1fd6786b2a17
Bart <bart.malanczuk@gmail.com>
2017-07-11 23:26:09 +0200

    Connect vue router

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import './style.scss';
-import Overview from './components/Overview.vue';
+import routes from './util/routes.js'
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import moment from 'moment-timezone';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, '$moment', { get: () => moment });
 
+const router = new VueRouter({
+    routes
+});
+
 new Vue({
     el: '#app',
-    components: {
-        Overview,
-    },
+    router,
 });
