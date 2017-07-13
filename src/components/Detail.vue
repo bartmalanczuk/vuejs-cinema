8e0f4287f47a8cc2bbd306d31d792e25651064c5
Bart <bart.malanczuk@gmail.com>
2017-07-13 20:57:29 +0200

    Flesh out detail page

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,32 @@
 <template>
     <div id="detail">
-        <movie-item v-if="movie" v-bind:movie="movie.movie"></movie-item>
+        <movie-item v-if="movie" v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr>
+                        <td>Released date:</td>
+                        <td>{{movie.Released}}</td>
+                    </tr>
+                    <tr>
+                        <td>Running time:</td>
+                        <td>{{movie.Runtime}}</td>
+                    </tr>
+                    <tr>
+                        <td>Director:</td>
+                        <td>{{movie.Director}}</td>
+                    </tr>
+                    <tr>
+                        <td>Cast:</td>
+                        <td>{{movie.Actors}}</td>
+                    </tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{name: 'home'}">Back to results</router-link>
+        </div>
     </div>
 </template>
 <script>
@@ -14,7 +40,7 @@
         },
         created() {
             this.$http.get('/api').then(response => {
-                this.movie = response.data.find((movie) => movie.movie.imdbID === this.$route.params.id);
+                this.movie = response.data.find((movie) => movie.movie.imdbID === this.$route.params.id).movie;
             });
         },
         components: {
